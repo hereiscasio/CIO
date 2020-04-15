43b306001205259d6231bc336afeab303a3dd2ad
Casio <hereiscasio@gmail.com>
2020-04-15 18:52:52 +0800

    refactor: 💡 remove deaded code

--- tests/e2e/support/clock_page.utils.js
+++ tests/e2e/support/clock_page.utils.js
@@ -4,16 +4,8 @@ const UID = `users/${Cypress.env('TEST_UID')}`
 
 export const remove_data_of_test_user_in_rtdb = function ()
 {
-	// const today = {
-	// 	date: format(Date.now(), 'yyyy-LL-dd'),
-	// 	monthWithYear: format(Date.now(), 'yyyy-LL')
-	// };
 	cy
-		.callRtdb(
-			'set',
-			UID,
-			null
-		);
+		.callRtdb('set', UID, null);
 };
 
 export const perform_clock_in = function ()
